96171eae596e6e533100c6de9f152945959681df
Cindy Qi Li <cli@ocad.ca>
2013-12-10 10:13:37 -0500

    FLOE-108: Improved the indicator component by naming state options with actual states instead of colours as well as removing the duplicate in indicator unit tests.

--- tests/js/indicatorTests.js
+++ tests/js/indicatorTests.js
@@ -17,43 +17,50 @@ https://github.com/gpii/universal/LICENSE.txt
 (function ($) {
     fluid.registerNamespace("fluid.tests");
 
-    jqUnit.test("Test metadata indicator", function () {
-        var that = fluid.metadata.indicator(".flc-indicator");
-        var tooltip = $("[id^=ui-tooltip]");
+    fluid.tests.addIndicatorListener = function (that, state) {
+        var expected = fluid.get(that.options.tooltipContent, state);
 
-        jqUnit.expect(4);
         that.tooltip.events.afterOpen.addListener(function () {
-            jqUnit.assertEquals("The tooltip content has been applied to the tooltip element", that.options.tooltipContent.unknown, $("[id^=ui-tooltip]").text());
+            jqUnit.assertEquals("The tooltip content has been applied to the tooltip element", expected, $("[id^=ui-tooltip]").text());
             that.tooltip.events.afterOpen.removeListener("checkContent");
         }, "checkContent", null, "last");
+    };
+
+    fluid.tests.checkInitial = function (that) {
+        var tooltip = $("[id^=ui-tooltip]");
         jqUnit.assertTrue("The initial circle css has been applied", that.container.hasClass(that.options.styles.circle));
-        jqUnit.assertTrue("The initial color css has been applied", that.container.hasClass(that.options.styles.indicatorColors.unknown));
         jqUnit.assertTrue("The tooltip css has been applied to the tooltip element", tooltip.hasClass(that.options.tooltipOptions.styles.tooltip));
         that.tooltip.open();
         that.tooltip.close();
+    };
 
-        jqUnit.expect(3);
-        that.tooltip.events.afterOpen.addListener(function () {
-            jqUnit.assertEquals("The tooltip content has been applied to the tooltip element", that.options.tooltipContent.available, $("[id^=ui-tooltip]").text());
-            that.tooltip.events.afterOpen.removeListener("checkContent");
-        }, "checkContent", null, "last");
-        that.applier.requestChange("value", "available");
-
-        jqUnit.assertFalse("The previous color css has been removed", that.container.hasClass(that.options.styles.indicatorColors.unknown));
-        jqUnit.assertTrue("The color to indicate 'available' state has been applied", that.container.hasClass(that.options.styles.indicatorColors.available));
+    fluid.tests.checkState = function (that, previousState, currentState) {
+        var previousStateCss = fluid.get(that.options.styles.indicatorState, previousState);
+        var currentStateCss = fluid.get(that.options.styles.indicatorState, currentState);
+        jqUnit.assertFalse("The previous color css has been removed", that.container.hasClass(previousStateCss));
+        jqUnit.assertTrue("The color to indicate 'available' state has been applied", that.container.hasClass(currentStateCss));
         that.tooltip.open();
         that.tooltip.close();
+    };
+
+    jqUnit.test("Test metadata indicator", function () {
+        var that = fluid.metadata.indicator(".flc-indicator");
 
         jqUnit.expect(3);
-        that.tooltip.events.afterOpen.addListener(function () {
-            jqUnit.assertEquals("The tooltip content has been applied to the tooltip element", that.options.tooltipContent.unavailable, $("[id^=ui-tooltip]").text());
-            that.tooltip.events.afterOpen.removeListener("checkContent");
-        }, "checkContent", null, "last");
-        that.applier.requestChange("value", "unavailable");
-        jqUnit.assertFalse("The previous color css has been removed", that.container.hasClass(that.options.styles.indicatorColors.available));
-        jqUnit.assertTrue("The color to indicate 'unavailable' state has been applied", that.container.hasClass(that.options.styles.indicatorColors.unavailable));
-        that.tooltip.open();
-        that.tooltip.close();
+        fluid.tests.addIndicatorListener(that, "unknown");
+        fluid.tests.checkInitial(that);
+
+        jqUnit.expect(3);
+        var currentState = "available";
+        fluid.tests.addIndicatorListener(that, currentState);
+        that.applier.requestChange("value", currentState);
+        fluid.tests.checkState(that, "unknown", currentState);
+
+        jqUnit.expect(3);
+        currentState = "unavailable";
+        fluid.tests.addIndicatorListener(that, currentState);
+        that.applier.requestChange("value", currentState);
+        fluid.tests.checkState(that, "available", currentState);
     });
 
 })(jQuery);
