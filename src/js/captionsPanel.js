bda9fcec7a5ff29785d003045c880040db30c4a3
Justin Obara <obarajustin@gmail.com>
2014-01-20 13:03:42 -0500

    FLOE-116: Updated captions with resourceInputPanel

--- src/js/captionsPanel.js
+++ src/js/captionsPanel.js
@@ -20,102 +20,47 @@ var fluid_1_5 = fluid_1_5 || {};
 
 (function ($, fluid) {
 
-    fluid.registerNamespace("fluid.metadata");
-
-    /*******************************************************************************
-     * The panel to define captions related metadata
-     *******************************************************************************/
+    /*************************************************
+     * The panel to define captions related metadata *
+     *************************************************/
 
     // todo: The creation of captionInput sub-components should be generalized, perhaps by
     // using dynamic components, due to their similarity.
     fluid.defaults("fluid.metadata.captionsPanel", {
-        gradeNames: ["fluid.rendererComponent", "autoInit"],
-        model: {
-            captions: [{
-                src: "",
-                language: "en"
-            }, {
-                src: "",
-                language: "en"
-            }]
-        },
+        gradeNames: ["fluid.metadata.resourceInputPanel", "autoInit"],
         components: {
-            primaryCaption: {
-                type: "fluid.metadata.captionsPanel.captionInput",
-                container: "{captionsPanel}.dom.primaryCaption",
-                createOnEvent: "afterRender",
+            primaryResource: {
+                createOnEvent: "afterMarkupReady",
                 options: {
-                    model: "{captionsPanel}.model.captions.0",
-                    events: {
-                        afterRender: "{captionsPanel}.events.afterRenderPrimaryCaption"
-                    },
-                    modelListeners: {
-                        "*": {
-                            func: "{captionsPanel}.events.inputModelChanged.fire",
-                            args: ["{change}.path", "{change}.value", 0]
-                        }
-                    }
+                    strings: "{captionsPanel}.options.strings.resources"
                 }
             },
-            secondaryCaption: {
-                type: "fluid.metadata.captionsPanel.captionInput",
-                container: "{captionsPanel}.dom.secondaryCaption",
-                createOnEvent: "afterRender",
+            secondaryResource: {
+                createOnEvent: "afterMarkupReady",
                 options: {
-                    model: "{captionsPanel}.model.captions.1",
-                    events: {
-                        afterRender: "{captionsPanel}.events.afterRenderSecondaryCaption"
-                    },
-                    modelListeners: {
-                        "*": {
-                            func: "{captionsPanel}.events.inputModelChanged.fire",
-                            args: ["{change}.path", "{change}.value", 1]
-                        }
-                    }
+                    strings: "{captionsPanel}.options.strings.resources"
                 }
             },
-            icon: {
-                type: "fluid.metadata.indicator",
-                container: "{captionsPanel}.dom.icon",
-                createOnEvent: "afterRender",
+            indicator: {
+                createOnEvent: "afterMarkupReady",
                 options: {
-                    model: {
-                        value: {
-                            expander: {
-                                funcName: "{captionsPanel}.getIconModelValue"
-                            }
-                        }
-                    },
-                    tooltipContent: {
-                        "available": "${captionsPanel}.options.strings.captionsAvailable",
-                        "unavailable": "${captionsPanel}.options.strings.captionsUnavailable"
-                    },
-                    events: {
-                        onCreate: {
-                            events: "{captionsPanel}.events.afterRenderIcon",
-                            priority: "last"
-                        }
-                    }
+                    tooltipContent: "{captionsPanel}.options.strings.tooltip"
                 }
             }
         },
         strings: {
             title: "Captions",
-            instruction: "Captions provide a synchronized, equivalent text version of spoken word in a video.",
-            captionsAvailable: "Captions are available in this video.",
-            captionsUnavailable: "Captions are not available in this video."
-        },
-        selectors: {
-            title: ".flc-captions-title",
-            icon: ".flc-captions-icon",
-            instruction: ".flc-captions-instruction",
-            primaryCaption: ".flc-captions-input-1",
-            secondaryCaption: ".flc-captions-input-2"
-        },
-        selectorsToIgnore: ["icon", "primaryCaption", "secondaryCaption"],
-        protoTree: {
-            title: {messagekey: "title"},
-            instruction: {messagekey: "instruction"}
+            description: "Captions provide a synchronized, equivalent text version of spoken word in a video.",
+            tooltip: {
+                captionsAvailable: "Captions are available in this video.",
+                captionsUnavailable: "Captions are not available in this video."
+            },
+            resources: {
+                srcLabel: "Enter web link to caption:",
+                languagesLabel: "Select language:",
+                srcPlaceholder: "www.example.com/movie.srt",
+                languages: ["Arabic", "Chinese", "English", "French", "Hindi", "Spanish"]
+            }
         },
         resources: {
             template: {
@@ -123,126 +68,18 @@ var fluid_1_5 = fluid_1_5 || {};
                 forceCache: true
             }
         },
-        invokers: {
-            getIconModelValue: {
-                funcName: "fluid.metadata.captionsPanel.getIconModelValue",
-                args: "{that}.model"
-            }
-        },
         events: {
-            inputModelChanged: null,
-            afterRenderPrimaryCaption: null,
-            afterRenderSecondaryCaption: null,
-            afterRenderIcon: null,
-            onReady: {
-                events: {
-                    onCreate: "onCreate",
-                    afterRenderPrimaryCaption: "afterRenderPrimaryCaption",
-                    afterRenderSecondaryCaption: "afterRenderSecondaryCaption",
-                    afterRenderIcon: "afterRenderIcon"
-                },
-                args: "{that}"
-            }
+            afterMarkupReady: null
         },
         listeners: {
-            "onCreate.init": "fluid.metadata.captionsPanel.init",
-            "inputModelChanged.updateModel": {
-                listener: "fluid.metadata.captionsPanel.updateModel",
-                args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2"]
-            }
-        },
-        modelListeners: {
-            "*": {
-                func: "fluid.metadata.captionsPanel.refreshIcon",
-                args: "{that}"
-            }
-        },
-        distributeOptions: [{
-            source: "{that}.options.captionsInputTemplate",
-            target: "{that > primaryCaption}.options.resources.template.url"
-        }, {
-            source: "{that}.options.captionsInputTemplate",
-            target: "{that > secondaryCaption}.options.resources.template.url"
-        }]
+            "onCreate.init": "fluid.metadata.captionsPanel.init"
+        }
     });
 
     fluid.metadata.captionsPanel.init = function (that) {
         fluid.fetchResources(that.options.resources, function (resourceSpec) {
-            that.refreshView();
-        });
-    };
-
-    fluid.metadata.captionsPanel.updateModel = function (captionsPanel, path, value, index) {
-        var captionsModel = fluid.get(captionsPanel.model, "captions");
-        fluid.set(captionsModel, [index, path[0]], value);
-        captionsPanel.applier.requestChange("captions", captionsModel);
-    };
-
-    fluid.metadata.captionsPanel.getIconModelValue = function (captionsModel) {
-        return fluid.find(captionsModel.captions, function (caption) {
-            if (caption.src && caption.src !== "") {
-                return "available";
-            }
-        }, "unavailable");
-    };
-
-    fluid.metadata.captionsPanel.refreshIcon = function (that) {
-        that.icon.applier.requestChange("value", that.getIconModelValue());
-    };
-
-    /*******************************************************************************
-     * The panel to render caption input fields: src and language
-     *******************************************************************************/
-
-    fluid.defaults("fluid.metadata.captionsPanel.captionInput", {
-        gradeNames: ["fluid.rendererComponent", "autoInit"],
-        selectors: {
-            srcLabel: ".flc-captions-srcLabel",
-            src: ".flc-captions-src",
-            languagesLabel: ".flc-captions-languagesLabel",
-            languages: ".flc-captions-languages"
-        },
-        strings: {
-            srcLabel: "Enter web link to caption:",
-            languagesLabel: "Select language:",
-            srcPlaceholder: "www.example.com/movie.srt",
-            languages: ["Arabic", "Chinese", "English", "French", "Hindi", "Spanish"]
-        },
-        model: {
-            src: "",
-            language: "en"
-        },
-        controlValues: ["ar", "zh", "en", "fr", "hi", "es"],
-        protoTree: {
-            srcLabel: {messagekey: "srcLabel"},
-            src: {
-                value: "${src}",
-                decorators: {
-                    type: "attrs",
-                    attributes: {placeholder: "${{that}.options.strings.srcPlaceholder}"}
-                }
-            },
-            languagesLabel: {messagekey: "languagesLabel"},
-            languages: {
-                "selection": "${language}",
-                "optionlist": "${{that}.options.controlValues}",
-                "optionnames": "${{that}.options.strings.languages}"
-            }
-        },
-        listeners: {
-            "onCreate.init": "fluid.metadata.captionsPanel.captionInput.init"
-        },
-        resources: {
-            template: {
-                url: "../html/captions-input-template.html",
-                forceCache: true
-            }
-        }
-    });
-
-    fluid.metadata.captionsPanel.captionInput.init = function (that) {
-        fluid.fetchResources(that.options.resources, function (resourceSpec) {
-            that.refreshView();
+            that.container.append(resourceSpec.template.resourceText);
+            that.events.afterMarkupReady.fire(that);
         });
     };
 
--- tests/js/captionsPanelTests.js
+++ tests/js/captionsPanelTests.js
@@ -17,89 +17,6 @@ https://github.com/gpii/universal/LICENSE.txt
 (function ($) {
     fluid.registerNamespace("fluid.tests");
 
-    fluid.defaults("fluid.tests.captionsInputTests", {
-        gradeNames: ["fluid.test.testEnvironment", "autoInit"],
-        components: {
-            captionsInput: {
-                type: "fluid.metadata.captionsPanel.captionInput",
-                container: ".flc-captionsPanel-captionsInput",
-                createOnEvent: "{captionsInputTester}.events.onTestCaseStart",
-                options: {
-                    resources: {
-                        template: {
-                            url: "../../src/html/captions-input-template.html"
-                        }
-                    }
-                }
-            },
-            captionsInputTester: {
-                type: "fluid.tests.captionsInputTester"
-            }
-        }
-    });
-
-    fluid.tests.checkInit = function (that) {
-        jqUnit.assertEquals("The placeholder for the input field is set", that.options.strings.srcPlaceholder, that.locate("src").attr("placeholder"));
-        jqUnit.assertEquals("All language options are rendered in a combo box", that.options.controlValues.length, that.locate("languages").find("option").length);
-    };
-
-    fluid.tests.changeSrc = function (that, newSrcValue) {
-        that.locate("src").val(newSrcValue).change();
-    };
-
-    fluid.tests.changeLanguage = function (that, newLanguageValue) {
-        that.locate("languages").find("[value='" + newLanguageValue + "']").attr("selected", "selected").change();
-    };
-
-    fluid.tests.checkModelValue = function (newSrcValue) {
-        return function (newModel, oldModel, changeRequest) {
-            var path = changeRequest[0].path;
-            jqUnit.assertEquals("The model path '" + path + "' has been updated to the new value", newSrcValue, fluid.get(newModel, path));
-        };
-    };
-
-    fluid.defaults("fluid.tests.captionsInputTester", {
-        gradeNames: ["fluid.test.testCaseHolder", "autoInit"],
-        testOptions: {
-            newSrcValue: "http://weblink.com/one.mp4",
-            newLanguage: "hi"
-        },
-        modules: [{
-            name: "Test captions input fields panel",
-            tests: [{
-                expect: 2,
-                name: "Init",
-                sequence: [{
-                    listener: "fluid.tests.checkInit",
-                    event: "{captionsInputTests captionsInput}.events.afterRender"
-                }]
-            }]
-        }, {
-            name: "Test captions input panel",
-            tests: [{
-                expect: 2,
-                name: "Click on captions input fields",
-                sequence: [{
-                    func: "fluid.tests.changeSrc",
-                    args: ["{captionsInput}", "{that}.options.testOptions.newSrcValue"]
-                }, {
-                    listenerMaker: "fluid.tests.checkModelValue",
-                    makerArgs: ["{that}.options.testOptions.newSrcValue"],
-                    spec: {path: "src", priority: "last"},
-                    changeEvent: "{captionsInput}.applier.modelChanged"
-                }, {
-                    func: "fluid.tests.changeLanguage",
-                    args: ["{captionsInput}", "{that}.options.testOptions.newLanguage"]
-                }, {
-                    listenerMaker: "fluid.tests.checkModelValue",
-                    makerArgs: ["{that}.options.testOptions.newLanguage"],
-                    spec: {path: "language", priority: "last"},
-                    changeEvent: "{captionsInput}.applier.modelChanged"
-                }]
-            }]
-        }]
-    });
-
     fluid.defaults("fluid.tests.captionsPanelTests", {
         gradeNames: ["fluid.test.testEnvironment", "autoInit"],
         components: {
@@ -113,7 +30,7 @@ https://github.com/gpii/universal/LICENSE.txt
                             url: "../../src/html/captions-template.html"
                         }
                     },
-                    captionsInputTemplate: "../../src/html/captions-input-template.html"
+                    resourceInputTemplate: "../../src/html/resourceInput-template.html"
                 }
             },
             captionsPanelTester: {
@@ -128,12 +45,12 @@ https://github.com/gpii/universal/LICENSE.txt
 
         jqUnit.assertEquals("Two src fields have been rendered", 2, srcFields.length);
         jqUnit.assertEquals("Two language fields have been rendered", 2, languageFields.length);
-        jqUnit.assertTrue("The icon state has been set to 'unavailable'", that.locate("icon").hasClass(that.icon.options.styles.indicatorState.unavailable));
+        jqUnit.assertTrue("The indicator state has been set to 'unavailable'", that.locate("indicator").hasClass(that.indicator.options.styles.indicatorState.unavailable));
         srcFields.each(function () {
-            jqUnit.assertEquals("The placeholder for the input field has been set", that.primaryCaption.options.strings.srcPlaceholder, $(this).attr("placeholder"));
+            jqUnit.assertEquals("The placeholder for the input field has been set", that.primaryResource.options.strings.srcPlaceholder, $(this).attr("placeholder"));
         });
         languageFields.each(function () {
-            jqUnit.assertEquals("All language options have been rendered in a combo box", that.primaryCaption.options.controlValues.length, $(this).find("option").length);
+            jqUnit.assertEquals("All language options have been rendered in a combo box", that.primaryResource.options.controlValues.length, $(this).find("option").length);
         });
     };
 
@@ -147,8 +64,8 @@ https://github.com/gpii/universal/LICENSE.txt
 
     fluid.tests.checkModelValueByIndex = function (that, path, newSrcValue, index) {
         return function (newModel, oldModel, changeRequest) {
-            jqUnit.assertEquals("The model path '" + path + "' has been updated to the new value", newSrcValue, fluid.get(newModel, ["captions", index, path]));
-            jqUnit.assertTrue("The icon state has been set to 'available'", that.locate("icon").hasClass(that.icon.options.styles.indicatorState.available));
+            jqUnit.assertEquals("The model path '" + path + "' has been updated to the new value", newSrcValue, fluid.get(newModel, ["resources", index, path]));
+            jqUnit.assertTrue("The indicator state has been set to 'available'", that.locate("indicator").hasClass(that.indicator.options.styles.indicatorState.available));
         };
     };
 
@@ -181,7 +98,7 @@ https://github.com/gpii/universal/LICENSE.txt
                 }, {
                     listenerMaker: "fluid.tests.checkModelValueByIndex",
                     makerArgs: ["{captionsPanel}", "src", "{that}.options.testOptions.newSrcValue1", 0],
-                    spec: {path: "captions", priority: "last"},
+                    spec: {path: "resources", priority: "last"},
                     changeEvent: "{captionsPanel}.applier.modelChanged"
                 }, {
                     func: "fluid.tests.changeLanguageByIndex",
@@ -189,7 +106,7 @@ https://github.com/gpii/universal/LICENSE.txt
                 }, {
                     listenerMaker: "fluid.tests.checkModelValueByIndex",
                     makerArgs: ["{captionsPanel}", "language", "{that}.options.testOptions.newLanguage1", 0],
-                    spec: {path: "captions", priority: "last"},
+                    spec: {path: "resources", priority: "last"},
                     changeEvent: "{captionsPanel}.applier.modelChanged"
                 }, {
                     func: "fluid.tests.changeSrcByIndex",
@@ -197,7 +114,7 @@ https://github.com/gpii/universal/LICENSE.txt
                 }, {
                     listenerMaker: "fluid.tests.checkModelValueByIndex",
                     makerArgs: ["{captionsPanel}", "src", "{that}.options.testOptions.newSrcValue2", 1],
-                    spec: {path: "captions", priority: "last"},
+                    spec: {path: "resources", priority: "last"},
                     changeEvent: "{captionsPanel}.applier.modelChanged"
                 }, {
                     func: "fluid.tests.changeLanguageByIndex",
@@ -205,7 +122,7 @@ https://github.com/gpii/universal/LICENSE.txt
                 }, {
                     listenerMaker: "fluid.tests.checkModelValueByIndex",
                     makerArgs: ["{captionsPanel}", "language", "{that}.options.testOptions.newLanguage2", 1],
-                    spec: {path: "captions", priority: "last"},
+                    spec: {path: "resources", priority: "last"},
                     changeEvent: "{captionsPanel}.applier.modelChanged"
                 }]
             }]
@@ -214,7 +131,6 @@ https://github.com/gpii/universal/LICENSE.txt
 
     $(document).ready(function () {
         fluid.test.runTests([
-            "fluid.tests.captionsInputTests",
             "fluid.tests.captionsPanelTests"
         ]);
     });
