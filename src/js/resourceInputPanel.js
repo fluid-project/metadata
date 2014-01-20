9dfedf2c8a90b2bdf2f761d43aecfb7ca7b87e49
Justin Obara <obarajustin@gmail.com>
2014-01-20 11:23:49 -0500

    FLOE-116: Added resourceInputPanel
    Added a panel grade for resource input.

---
+++ src/js/resourceInputPanel.js
@@ -0,0 +1,230 @@
+/*
+Copyright 2013 OCAD University
+Copyright 2014 OCAD University
+
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://github.com/fluid-project/infusion/raw/master/Infusion-LICENSE.txt
+*/
+
+// Declare dependencies
+/*global fluid_1_5:true, jQuery*/
+
+// JSLint options
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+var fluid_1_5 = fluid_1_5 || {};
+
+
+(function ($, fluid) {
+
+    fluid.registerNamespace("fluid.metadata");
+
+    /******************
+     * Resource Input *
+     ******************/
+
+    fluid.defaults("fluid.metadata.resourceInput", {
+        gradeNames: ["fluid.rendererComponent", "autoInit"],
+        selectors: {
+            srcLabel: ".flc-resourceInput-srcLabel",
+            src: ".flc-resourceInput-src",
+            languagesLabel: ".flc-resourceInput-languagesLabel",
+            languages: ".flc-resourceInput-languages"
+        },
+        strings: {
+            srcLabel: "Enter web link to caption:",
+            languagesLabel: "Select language:",
+            srcPlaceholder: "www.example.com/movie.srt",
+            languages: ["Arabic", "Chinese", "English", "French", "Hindi", "Spanish"]
+        },
+        model: {
+            src: "",
+            language: "en"
+        },
+        controlValues: ["ar", "zh", "en", "fr", "hi", "es"],
+        protoTree: {
+            srcLabel: {messagekey: "srcLabel"},
+            src: {
+                value: "${src}",
+                decorators: {
+                    type: "attrs",
+                    attributes: {placeholder: "${{that}.options.strings.srcPlaceholder}"}
+                }
+            },
+            languagesLabel: {messagekey: "languagesLabel"},
+            languages: {
+                "selection": "${language}",
+                "optionlist": "${{that}.options.controlValues}",
+                "optionnames": "${{that}.options.strings.languages}"
+            }
+        },
+        listeners: {
+            "onCreate.init": "fluid.metadata.resourceInput.init"
+        },
+        resources: {
+            template: {
+                url: "../html/resourceInput-template.html",
+                forceCache: true
+            }
+        }
+    });
+
+    fluid.metadata.resourceInput.init = function (that) {
+        fluid.fetchResources(that.options.resources, function (resourceSpec) {
+            that.refreshView();
+        });
+    };
+
+    /************************************
+     * conditionAny transformation spec *
+     ************************************/
+
+    fluid.defaults("fluid.transforms.conditionAny", {
+        gradeNames: [ "fluid.multiInputTransformFunction", "fluid.standardOutputTransformFunction" ]
+    });
+
+    fluid.transforms.conditionAny = function (inputs, transformSpec, transform) {
+
+        var conditions = transformSpec.conditionPath ? fluid.get(transform.source, transformSpec.conditionPath) : transformSpec.condition;
+        var innerPath = transformSpec.innerPath || "";
+        for (var i=0; i <= conditions.length; i++) {
+            var condition = conditions[i];
+            if (fluid.get(condition, innerPath)) {
+                return transformSpec["true"];
+            }
+        }
+
+        return transformSpec["false"];
+    };
+
+    /************************
+     * Resource input panel *
+     ************************/
+
+    fluid.defaults("fluid.metadata.baseResourceInputPanel", {
+        gradeNames: ["fluid.metadata.panel", "autoInit"],
+        selectors: {
+            primaryResource: ".flc-resourceInputPanel-primary",
+            secondaryResource: ".flc-resourceInputPanel-secondary",
+        },
+
+        model: {
+            resources: [{
+                src: "",
+                language: "en"
+            }, {
+                src: "",
+                language: "en"
+            }]
+        },
+        components: {
+            //TODO: use dynamic components instead of hard coding two resources
+            primaryResource: {
+                type: "fluid.metadata.resourceInput",
+                container: "{that}.dom.primaryResource",
+                options: {
+                    model: {
+                        expander: {
+                            funcName: "fluid.copy",
+                            args: "{baseResourceInputPanel}.model.resources.0"
+                        }
+                    },
+                    modelListeners: {
+                        "*": {
+                            func: "{baseResourceInputPanel}.updateModel",
+                            args: ["{change}.value", "{change}.path", 0]
+                        }
+                    },
+                    listeners: {
+                        afterRender: "{baseResourceInputPanel}.events.afterRenderPrimaryCaption"
+                    }
+                }
+            },
+            secondaryResource: {
+                type: "fluid.metadata.resourceInput",
+                container: "{that}.dom.secondaryResource",
+                options: {
+                    model: {
+                        expander: {
+                            funcName: "fluid.copy",
+                            args: "{baseResourceInputPanel}.model.resources.1"
+                        }
+                    },
+                    modelListeners: {
+                        "*": {
+                            func: "{baseResourceInputPanel}.updateModel",
+                            args: ["{change}.value", "{change}.path", 1]
+                        }
+                    },
+                    listeners: {
+                        afterRender: "{baseResourceInputPanel}.events.afterRenderSecondaryCaption"
+                    }
+                }
+            }
+        },
+        events: {
+            afterRenderPrimaryCaption: null,
+            afterRenderSecondaryCaption: null,
+            onReady: {
+                events: {
+                    onCreate: "onCreate",
+                    afterRenderPrimaryCaption: "afterRenderPrimaryCaption",
+                    afterRenderSecondaryCaption: "afterRenderSecondaryCaption",
+                },
+                args: "{that}"
+            }
+        },
+        invokers: {
+            updateModel: {
+                funcName: "fluid.metadata.baseResourceInputPanel.updateModel",
+                args: ["{that}", "{arguments}.0", "{arguments}.1", "{arguments}.2", "resources"],
+                dynamic: true
+            }
+        },
+        indicatorModelRules: {
+            value: {
+                transform: {
+                    type: "fluid.transforms.conditionAny",
+                    conditionPath: "resources",
+                    innerPath: "src",
+                    "true": "available",
+                    "false": "unavailable"
+                }
+            }
+        },
+        distributeOptions: [{
+            source: "{that}.options.resourceInputTemplate",
+            target: "{that > primaryResource}.options.resources.template.url"
+        }, {
+            source: "{that}.options.resourceInputTemplate",
+            target: "{that > secondaryResource}.options.resources.template.url"
+        }]
+    });
+
+    fluid.metadata.baseResourceInputPanel.updateModel = function (that, value, path, index, root) {
+        var changePath = [root, index, path].join(".");
+        that.applier.requestChange(changePath, value);
+    };
+
+    fluid.defaults("fluid.metadata.resourceInputPanel", {
+        gradeNames: ["fluid.metadata.baseResourceInputPanel", "autoInit"],
+        selectors: {
+            description: ".flc-resourceInputPanel-description"
+        },
+        strings: {
+            description: ""
+        },
+        listeners: {
+            "onCreate.writeDescription": {
+                "this": "{that}.dom.description",
+                "method": "text",
+                "args": "{that}.options.strings.description",
+            }
+        }
+    });
+
+})(jQuery, fluid_1_5);
---
+++ tests/js/resourceInputPanelTests.js
@@ -0,0 +1,211 @@
+/*!
+Copyright 2014 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+// Declare dependencies
+/*global fluid, jqUnit, expect, jQuery*/
+
+// JSLint options
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+(function ($) {
+    fluid.registerNamespace("fluid.tests");
+
+    fluid.defaults("fluid.tests.resourceInputTests", {
+        gradeNames: ["fluid.test.testEnvironment", "autoInit"],
+        components: {
+            resourceInput: {
+                type: "fluid.metadata.resourceInput",
+                container: ".flc-resourceInput",
+                createOnEvent: "{resourceInputTester}.events.onTestCaseStart",
+                options: {
+                    resources: {
+                        template: {
+                            url: "../../src/html/resourceInput-template.html"
+                        }
+                    }
+                }
+            },
+            resourceInputTester: {
+                type: "fluid.tests.resourceInputTester"
+            }
+        }
+    });
+
+    fluid.tests.checkInit = function (that) {
+        jqUnit.assertEquals("The placeholder for the input field is set", that.options.strings.srcPlaceholder, that.locate("src").attr("placeholder"));
+        jqUnit.assertEquals("All language options are rendered in a combo box", that.options.controlValues.length, that.locate("languages").find("option").length);
+    };
+
+    fluid.tests.changeSrc = function (that, newSrcValue) {
+        that.locate("src").val(newSrcValue).change();
+    };
+
+    fluid.tests.changeLanguage = function (that, newLanguageValue) {
+        that.locate("languages").find("[value='" + newLanguageValue + "']").attr("selected", "selected").change();
+    };
+
+    fluid.tests.checkModelValue = function (newSrcValue) {
+        return function (newModel, oldModel, changeRequest) {
+            var path = changeRequest[0].path;
+            jqUnit.assertEquals("The model path '" + path + "' has been updated to the new value", newSrcValue, fluid.get(newModel, path));
+        };
+    };
+
+    fluid.defaults("fluid.tests.resourceInputTester", {
+        gradeNames: ["fluid.test.testCaseHolder", "autoInit"],
+        testOptions: {
+            newSrcValue: "http://weblink.com/one.mp4",
+            newLanguage: "hi"
+        },
+        modules: [{
+            name: "Test resource input fields panel",
+            tests: [{
+                expect: 2,
+                name: "Init",
+                sequence: [{
+                    listener: "fluid.tests.checkInit",
+                    event: "{resourceInputTests resourceInput}.events.afterRender"
+                }]
+            }]
+        }, {
+            name: "Test resource input panel",
+            tests: [{
+                expect: 2,
+                name: "Click on resource input fields",
+                sequence: [{
+                    func: "fluid.tests.changeSrc",
+                    args: ["{resourceInput}", "{that}.options.testOptions.newSrcValue"]
+                }, {
+                    listenerMaker: "fluid.tests.checkModelValue",
+                    makerArgs: ["{that}.options.testOptions.newSrcValue"],
+                    spec: {path: "src", priority: "last"},
+                    changeEvent: "{resourceInput}.applier.modelChanged"
+                }, {
+                    func: "fluid.tests.changeLanguage",
+                    args: ["{resourceInput}", "{that}.options.testOptions.newLanguage"]
+                }, {
+                    listenerMaker: "fluid.tests.checkModelValue",
+                    makerArgs: ["{that}.options.testOptions.newLanguage"],
+                    spec: {path: "language", priority: "last"},
+                    changeEvent: "{resourceInput}.applier.modelChanged"
+                }]
+            }]
+        }]
+    });
+
+    jqUnit.asyncTest("baseResourceInputPanel", function () {
+        jqUnit.expect(3);
+
+        var newModel = {
+            resources: [{
+                src: "http://example.com/primary/video.mp4",
+                language: "en"
+            }, {
+                src: "http://example.com/secondary/video.mp4",
+                language: "en"
+            }]
+        };
+
+        var that = fluid.metadata.baseResourceInputPanel(".flc-baseResourceInputPanel", {
+            resourceInputTemplate: "../../src/html/resourceInput-template.html",
+            listeners: {
+                onReady: function (that) {
+                    // initial
+                    jqUnit.assertEquals("The initial indicator model should be set correctly", "unavailable", that.indicator.model.value);
+
+                    // model update
+                    that.primaryResource.applier.requestChange("src", "http://example.com/primary/video.mp4");
+                    that.secondaryResource.applier.requestChange("src", "http://example.com/secondary/video.mp4");
+                    jqUnit.assertDeepEq("The parent model should have been updated.", newModel, that.model);
+                    jqUnit.assertEquals("The indicator model should be updated correctly", "available", that.indicator.model.value);
+
+                    // start
+                    jqUnit.start();
+                }
+            }
+        });
+    });
+
+    jqUnit.asyncTest("resourceInputPanel", function () {
+        jqUnit.expect(1);
+
+        var description = "Test Description";
+
+        var that = fluid.metadata.resourceInputPanel(".flc-resourceInputPanel", {
+            resourceInputTemplate: "../../src/html/resourceInput-template.html",
+            strings: {
+                description: description
+            },
+            listeners: {
+                onReady: function (that) {
+                    // initial
+                    jqUnit.assertEquals("The description should have been rendered", description, that.locate("description").text());
+
+                    // start
+                    jqUnit.start();
+                }
+            }
+        });
+    });
+
+    jqUnit.test("transformation", function () {
+
+        var modelTrue = {
+            resources: [{
+                src: "",
+                language: "en"
+            }, {
+                src: "www.example.com/video.mp4",
+                language: "fr"
+            }]
+        };
+
+        var modelFalse = {
+            resources: [{
+                src: "",
+                language: "en"
+            }, {
+                src: "",
+                language: "fr"
+            }]
+        };
+
+        var rules = {
+            value: {
+                transform: {
+                    type: "fluid.transforms.conditionAny",
+                    conditionPath: "resources",
+                    innerPath: "src",
+                    "true": "available",
+                    "false": "unavailable"
+                }
+            }
+        };
+
+        var expectedTrue = {
+            value: "available"
+        };
+
+        var expectedFalse = {
+            value: "unavailable"
+        };
+
+        jqUnit.assertDeepEq("The model should be transformed", expectedTrue, fluid.model.transform(modelTrue, rules));
+        jqUnit.assertDeepEq("The model should be transformed", expectedFalse, fluid.model.transform(modelFalse, rules));
+
+    });
+
+    $(document).ready(function () {
+        fluid.test.runTests([
+            "fluid.tests.resourceInputTests"
+        ]);
+    });
+
+})(jQuery);
