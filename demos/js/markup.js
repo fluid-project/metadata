328e953acaf867ada8df372789c0da448f1ca4a6
Justin Obara <obarajustin@gmail.com>
2014-03-07 09:59:02 -0500

    FLOE-173: correcting copyrights

--- demos/js/markup.js
+++ demos/js/markup.js
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2013 OCAD University
+Copyright 2013-2014 OCAD University
 
 Licensed under the Educational Community License (ECL), Version 2.0 or the New
 BSD license. You may not use this file except in compliance with one these
