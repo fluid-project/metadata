6633daf2646623ea03a537bf2ce055c5973285c3
Cindy Qi Li <cli@ocad.ca>
2014-07-18 14:00:02 -0400

    FLOE-194: Added more unit tests.

---
+++ tests/components/js/happyPanelTests.js
@@ -0,0 +1,27 @@
+/*!
+Copyright 2014 OCAD University
+
+Licensed under the New BSD license. You may not use this file except in
+compliance with this License.
+
+You may obtain a copy of the License at
+https://github.com/gpii/universal/LICENSE.txt
+*/
+
+// Declare dependencies
+/*global fluid, jqUnit, expect, jQuery*/
+
+// JSLint options
+/*jslint white: true, funcinvoke: true, undef: true, newcap: true, nomen: true, regexp: true, bitwise: true, browser: true, forin: true, maxerr: 100, indent: 4 */
+
+(function ($) {
+    fluid.registerNamespace("fluid.tests.happyPanel");
+
+    $(document).ready(function () {
+        jqUnit.test("Test happyPanel", function () {
+            jqUnit.expect(1);
+            var that = gpii.metadata.feedback.happyPanel(".gpiic-panel");
+            jqUnit.assertEquals("The correct content is rendered", that.options.strings.content, that.container.text());
+        });
+    });
+})(jQuery);
